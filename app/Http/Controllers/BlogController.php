57fa218ea49f04d241f92e5f327e2b7309ab2113
Lucile ROSELLINI <l.rosellini@cerberis.com>
2020-09-23 15:52:12 +0200

    'v0'

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\blog;
+use Illuminate\Http\Request;
+use App\Models\blogModel as blogModel;
+
+class BlogController extends Controller
+{
+    public function affichetruc()
+    {
+        return view('truc');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articleBlog = blogModel::all();
+        $articleBlog->fresh();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(blog $blog)
+    {
+        //
+    }
+}
